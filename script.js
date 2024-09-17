c6b5b2b43fa1eb19bcb14370842070ee6c169d23
Taha258 <159728072+Taha258@users.noreply.github.com>
2024-09-17 06:37:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,148 @@
+
+// .......................Chapter 1.............................
+
+//Question 1
+
+var vari = alert("Welcome to our Website.");
+
+//Question 2
+
+var useService = alert("How are you?")
+
+//Question 3
+
+alert("what is you name?")
+alert("which class do you study")
+alert("what does do your father")
+
+//Question 4
+
+var save = alert("how are you?")
+console.log(save);
+
+// .......................Chapter 2.............................
+
+//Question 1
+
+var declere ;
+
+//Question 2
+
+var myName = "Taha Hussain";
+
+//Question 3
+
+var value = "Hello World!"
+
+alert(value)
+
+//Question 4
+
+var studentName = "Taha Hussain";
+var studentAge = "20";
+var studentCertified = "Application";
+
+alert(studentName)
+alert(studentAge)
+alert(studentCertified)
+
+//Question 5
+
+
+var onevarible = "I \n Am \n Fine \n And \n You"
+
+alert(onevarible)
+
+//Question 6
+
+var email = "muntahahussainsample@gmail.com";
+
+alert("My email address is" +' '+ email)
+
+//Question 7
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// .......................Chapter 3.............................
+
+//Question 1
+
+var age = 20;
+alert(age)
+
+
+//Question 2
+
+alert("You have visited this site 14 times")
+
+//Question 3
+
+var birthYear  = 2003;
+var message = "My birth year is "
+alert(birthYear+' '+ message)
+
+// .......................Chapter 4.............................
+
+//Question 1
+
+var one = "This is statement"
+var Two = "This is statement"
+var Three = "This is statement"
+
+//Question 2
+
+// Legal 
+
+var $variable ; 
+var _variable ; 
+var variable ; 
+var variableName ; 
+var variable_Name ; 
+
+// illegal 
+
+// var 1variable;
+// var #variable;
+// var variable Name;
+// var variable-Name;
+// var %variable-Name;
+
+// .......................Chapter 5.............................
+
+//Question 1
+
+var num1 = 2;
+var num2 = 3;
+var c = num1 + num2
+
+console.log(c);
+
+//Question 2
+
+// sub
+var num1 = 3;
+var num2 = 2;
+var c = num1 - num2
+
+console.log(c);
+
+//Mul
+var num1 = 3;
+var num2 = 2;
+var c = num1 * num2
+
+console.log(c);
+
+//Div
+var num1 = 10;
+var num2 = 2;
+var c = num1 / num2
+
+console.log(c);
+
+//Modu
+var num1 = 5;
+var num2 = 2;
+var c = num1 % num2
+
+console.log(c);
